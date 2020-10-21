ab22a8f6d5b444d095b3170bd3e06d37a634cbe9
Tong Yu <yutongsf@gmail.com>
2020-10-20 20:30:14 -0700

    543 tree easy

---
+++ 543_Diameter_of_Binary_Tree.js
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/diameter-of-binary-tree/
+// Easy
+// Frequet
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree = function (root) {
+  let diameter = 0;
+
+  dfs(root);
+
+  return diameter;
+
+  function dfs(node, level) {
+    if (!node) return 0;
+
+    const left = dfs(node.left);
+    const right = dfs(node.right);
+
+    // update diameter at every node
+    diameter = Math.max(diameter, left + right);
+
+    // update the largest number of edge so far
+    return 1 + Math.max(left, right);
+  }
+};
